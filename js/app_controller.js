dd83981bc13eb0b6c4c93d0d17e6fa13d50fa075
Paulo Tosi <tosi.paulo@gmail.com>
2016-07-14 19:04:06 -0300

    Criando crud no localstorage

---
+++ js/app_controller.js
@@ -0,0 +1,80 @@
+(function(){
+	'use strict'
+	
+	angular.module('app')
+	.controller('indexController', indexController);
+
+
+	function indexController($scope, $localStorage, listLocalStorage) {
+		var vm = this;
+		vm.nomes = [];
+		vm.id = vm.nomes.id || -1;
+
+	
+		/*vm.saveData = function(data) {
+
+			if($localStorage.message){
+				obj = $localStorage.message;
+				obj.push(data);
+			}else {
+				obj.push(data);
+				$localStorage.message = obj;
+			}
+
+		}*/
+
+
+
+		vm.saveData = function(data) {
+			vm.id++;
+			data.id = vm.id;
+			if(listLocalStorage.get()){
+				vm.nomes = listLocalStorage.get();
+
+				vm.nomes.push(angular.copy(data));	
+			}else {
+				vm.nomes.push(angular.copy(data));	
+			}
+
+			listLocalStorage.post(vm.nomes);
+			$scope.main.pessoa = '';
+			
+
+		}
+
+		vm.loadData = function() {
+
+			vm.nomes = listLocalStorage.get() || vm.nomes; 
+		}
+
+		vm.editar = function(data) {
+			$scope.main.pessoa = angular.copy(data);
+		}
+
+		vm.deletar = function(id) {
+			vm.nomes = vm.nomes.filter(function(element){ 
+				return element.id !== id;
+			})
+			vm.id++;
+			listLocalStorage.post(vm.nomes);
+		}
+
+		vm.atualizar = function(data) {
+			var nome = vm.nomes.map(function(el, i){
+				if(el.id === data.id) {
+					return data
+				}
+				return el;
+			});
+
+			vm.nomes = angular.copy(nome);
+			//vm.id++;
+			listLocalStorage.post(vm.nomes);
+			$scope.main.pessoa = '';
+		}
+
+		vm.loadData();
+	}
+
+
+})()
